5b4ce33b5f2750ccfe6c810d260a1904aac0df84
Mateusz Boś <mateuszbos@gmail.com>
2015-02-24 02:21:28 +0100

    Working cart

--- ProjektPPPPAI/urls.py
+++ ProjektPPPPAI/urls.py
@@ -9,9 +9,15 @@ urlpatterns = patterns('',
                        # url(r'^$', 'ProjektPPPPAI.views.home', name='home'),
                        url(r'.*.html', 'hurtownia.views.index'),
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^item/(?P<item>.*)', 'hurtownia.views.item_view'),
+                       url(r'^item/(?P<item>.+)', 'hurtownia.views.item_view'),
                        url(r'^category/(?P<cat_id>\d+)/(?P<cat_slug>.*)',
                            'hurtownia.views.category_view'),
+                       url(r'^cart/add/(?P<item_id>\d+)/(?P<amount>\d+)/$',
+                           'hurtownia.views.add_to_cart'),
+                       url(r'^cart/remove/(?P<item_id>\d+)/$', 'hurtownia.views.remove_from_cart'),
+                       url(r'^cart/remove/(?P<item_id>\d+)/(?P<amount>\d+)/$',
+                           'hurtownia.views.remove_from_cart'),
+                       url(r'^cart/$', 'hurtownia.views.cart_view'),
                        url(r'^login/$', 'django.contrib.auth.views.login',
                            {'template_name': 'hurtownia/login.html'}),
                        url(r'^logout/$', 'hurtownia.views.logout_view'),
@@ -20,8 +26,8 @@ urlpatterns = patterns('',
                            {'template_name': 'hurtownia/password.html',
                             'post_change_redirect': '/done/'}),
                        url(r'^done/', 'hurtownia.views.password_done'),
-                       url(r'^account/(?P<action>.*)', 'hurtownia.views.account_change_view'),
-                       url(r'^user/(?P<user_name>.*)', 'hurtownia.views.user_view'))
+                       url(r'^account/(?P<action>.+)', 'hurtownia.views.account_change_view'),
+                       url(r'^user/(?P<user_name>.+)', 'hurtownia.views.user_view'))
 
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
--- hurtownia/views.py
+++ hurtownia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import logout
 from django.contrib.auth.views import login
@@ -8,20 +10,6 @@ from models import Item
 from models import Category
 from helper import get_categories_list
 from ItemHelper import get_listing_by_category
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # Create your views here.
 
@@ -105,4 +93,102 @@ def user_view(request, user_name):
     # now return the rendered template
     return render(request, 'hurtownia/user.html',
                   {'categories': get_categories_list()})
+
+
+def cart_view(request):
+    if request.session.__contains__('cart'):
+
+        session_cart = request.session['cart']
+        temp_cart = []
+        cart = []
+        if session_cart:
+            temp_cart = breakdown_list_to_map(session_cart)
+        if temp_cart:
+            for t in temp_cart:
+                cart.append((Item.objects.filter(id=t[0])[0], t[1]))
+
+        return render(request, 'hurtownia/cart.html',
+                      {'cart': cart})
+    else:
+        return redirect(request.META['HTTP_REFERER'])
+
+
+def remove_from_cart(request, item_id, amount=0):
+    iamount = int(amount)
+    litem_id = long(item_id)
+    session_cart = request.session['cart']
+    temp_cart = []
+    if session_cart:
+        temp_cart = breakdown_list_to_map(session_cart)
+        if iamount == 0:
+            temp_cart = remove_from_list_by_id(temp_cart, litem_id)
+        else:
+            temp_cart = remove_from_list_with_amount(temp_cart, litem_id, iamount)
+        request.session['cart'] = temp_cart
+    return redirect(request.META['HTTP_REFERER'])
+
+
+def remove_from_list_with_amount(cart, item_id, amount):
+    old_t = ()
+    for t in cart:
+        if t[0] == item_id:
+            old_t = t
+    if old_t:
+        cart.remove(old_t)
+    if old_t[1] > amount:
+        cart.append((old_t[0], old_t[1] - amount))
+    return map_to_list(cart)
+
+
+def add_to_cart(request, item_id, amount):
+    iamount = int(amount)
+    litem_id = long(item_id)
+    if not request.session.__contains__('cart'):
+        request.session['cart'] = [litem_id, iamount]
+    else:
+        request.session['cart'] = add_to_list(request.session['cart'], litem_id, iamount)
+    return redirect(request.META['HTTP_REFERER'])
+
+
+def remove_from_list_by_id(my_map, my_id):
+    old_t = ()
+    for t in my_map:
+        if t[0] == my_id:
+            old_t = t
+    if old_t:
+        my_map.remove(old_t)
+    return map_to_list(my_map)
+
+
+def add_to_list(session_list, item, amount):
+    my_map = breakdown_list_to_map(session_list)
+    old_t = ()
+    for t in my_map:
+        if t[0] == item:
+            old_t = t
+    if old_t:
+        my_map.remove(old_t)
+    else:
+        old_t = (item, 0)
+    my_map.append((old_t[0], old_t[1] + amount))
+
+    return map_to_list(my_map)
+
+
+def breakdown_list_to_map(flat_map):
+    normal_map = []
+    length = len(flat_map)
+    i = 0
+    while i < length:
+        normal_map.append((flat_map[i], flat_map[i + 1]))
+        i += 2
+    return normal_map
+
+
+def map_to_list(normal_map):
+    flatten_map = []
+    for t in normal_map:
+        flatten_map.append(t[0])
+        flatten_map.append(t[1])
+    return flatten_map
 
